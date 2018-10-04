decfd6d39d46909a6ab2e4d7cbb2e7bdccdd505b
n3legal <nikita.shachnov@yandex.ru>
2018-10-04 20:12:17 +0300

    1.3

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i ++){
+            float res = 1 / ((float)(i) + 1);
+            System.out.println(res + " or 1/" + (i + 1));
+        }
+    }
+}
